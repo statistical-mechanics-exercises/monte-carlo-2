1b8b247f46c15305b48d4a505fe6eba5c72269b6
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-17 17:23:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def circle_estimate(N) : 
+  # Your code goes here
+  estimate = 0 
+  return estimate
+  
+# Three estimates for the area of the circle based on a random grid
+# of 1000 points are printed here
+print( circle_estimate(1000), circle_estimate(1000), circle_estimate(1000) )
