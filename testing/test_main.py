932f0bb27275dc9c5bc2edf346712210ea761d2c
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 15:55:07 +0000

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -1,9 +1,10 @@
 import unittest
+import scipy.stats
 from main import *
 
 class UnitTests(unittest.TestCase) :
     def test_estimate(self) : 
-       for j in range(10) : 
+       for j in range(1,10) : 
            nvars = j*100
-           mean, bar = np.pi/4, np.sqrt( (np.pi/4)*(1-(np.pi/4)) / nvars )*st.norm.ppf( (0.99 + 1) / 2 )
+           mean, bar = np.pi/4, np.sqrt( (np.pi/4)*(1-(np.pi/4)) / nvars )*scipy.stats.norm.ppf( (0.99 + 1) / 2 )
            self.assertTrue( np.abs(circle_estimate(nvars) - mean)<bar ) 
