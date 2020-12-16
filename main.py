364be912c8c263e1eb3396d1431bac2ebc3b3472
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 15:55:41 +0000

    Added answers

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import numpy as np
 def circle_estimate(N) : 
   # Your code goes here
   estimate = 0 
-  return estimate
+  for i in range(N) : 
+      x = np.random.uniform(0,1)
+      y = np.random.uniform(0,1)
+      if x*x + y*y < 1 : estimate = estimate + 1
+  return estimate / N
   
 # Three estimates for the area of the circle based on a random grid
 # of 1000 points are printed here
